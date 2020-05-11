e65fc9368386d85808b8999c52296497f3724a57
johncribb1 <johncribb1@gmail.com>
2020-05-11 22:43:03 +0100

    Random password generation function created.

--- password_generator.py
+++ password_generator.py
@@ -6,6 +6,21 @@ letters_up_and_down = string.ascii_letters
 numbers = string.digits
 symbols = string.punctuation
 
-print(letters_up_and_down)
-print(numbers)
-print(symbols)
+
+#Gains the length of the password required from user
+def password_length():
+    length = input('How long do you want your password?')
+    return int(length)
+
+#Generates random password based on the length specified
+def password_generator(length=15):
+    
+    strung_together = f'{letters_up_and_down}{numbers}{symbols}'
+    strung_together = list(strung_together)
+    random.shuffle(strung_together)
+
+    random_password = random.choices(strung_together, k=length)
+    random_password = ''.join(random_password)
+    return random_password
+
+print(password_generator(25))

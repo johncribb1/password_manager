d2af866c98111a0c6e1a470fe1abbda5a6d496ff
johncribb1 <johncribb1@gmail.com>
2020-05-11 22:23:03 +0100

    All the letters and numbers for generating our passwords have been obtained.

---
+++ password_generator.py
@@ -0,0 +1,11 @@
+import string
+import random
+
+#Gaining all the letters and numbers for generating our passwords
+letters_up_and_down = string.ascii_letters
+numbers = string.digits
+symbols = string.punctuation
+
+print(letters_up_and_down)
+print(numbers)
+print(symbols)

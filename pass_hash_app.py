0d6cf2217d4ec1fa3e123ae8615e8be704cc9d15
johncribb1 <johncribb1@gmail.com>
2020-05-20 00:00:40 +0100

    Deleted unused files, encrypt & decrypt functions completed.

--- pass_hash_app.py
+++ pass_hash_app.py
@@ -5,6 +5,7 @@ import os
 import string
 import random
 
+
 app_color = '#ffffff'
 PAGE_FONT = ('Roboto')
 #Gaining all the letters and numbers for generating our passwords
@@ -12,7 +13,6 @@ letters_up_and_down = string.ascii_letters
 numbers = string.digits
 symbols = string.punctuation
 
-logged_in_name = ' '
 
 class PassHashApp(tk.Tk):
     def __init__(self, *args, **kwargs):
@@ -20,6 +20,7 @@ class PassHashApp(tk.Tk):
 
         tk.Tk.wm_title(self, 'PassHash - Password Manager')
 
+        self.logged_in = tk.StringVar()
 
         container = tk.Frame(self)
         container.pack(side='top', fill='both', expand=True)
@@ -29,7 +30,7 @@ class PassHashApp(tk.Tk):
 
         self.frames = {}
 
-        for F in (LoginPage, RegisterPage, StartPage, PasswordGenerator, SetPassPage, PasswordVault):
+        for F in (LoginPage, RegisterPage, StartPage, PasswordGenerator, KeepPassPage, PasswordVault, AddLoginPage):
 
             frame = F(container, self)
 
@@ -70,7 +71,6 @@ class PassHashApp(tk.Tk):
         user_choice += symbols if value[2] else ''
         return user_choice
 
-
     def run_generator(self, number_check, letter_check, symbol_check, length, password_text):
         if __name__ == '__main__':
             choice_list = self.password_combination(number_check, letter_check, symbol_check)
@@ -83,33 +83,24 @@ class PassHashApp(tk.Tk):
             passwordchoice = tk.Label(self, text='Not run from main file, security breach.', bg=app_color, font=PAGE_FONT)
             passwordchoice.pack()
 
-    def reg_user(self, username, password, user_entry, pass_entry):
+    def reg_user(self, username, password, user_entry, pass_entry, register_message):
         username_info = username.get().encode('UTF-8')
         password_info = password.get().encode('UTF-8')
         
-        sec_key = ''
-        for i in password.get():
-            char = chr(ord(i) - 20)
-            sec_key += char
-        
-        
-
         hashed = bcrypt.hashpw(password_info, bcrypt.gensalt())
 
         file = open(username_info, 'wb')
         file.write(hashed)
         file.close()
         file2 = open(username.get() + 'li', 'w')
-        file2.write(str(sec_key))
         file2.close()
 
         user_entry.delete(0, 100)
         pass_entry.delete(0, 100)
 
-        success_label = tk.Label(self, text='Registration Success')
-        success_label.pack()
+        register_message.set('Registration Success')
 
-    def login_verify(self, username, password, user_entry, pass_entry,user_message):
+    def login_verify(self, username, password, user_entry, pass_entry, user_message):
         username_info = username.get()
         password_info = password.get().encode('UTF-8')
 
@@ -119,20 +110,19 @@ class PassHashApp(tk.Tk):
         list_of_files = os.listdir()
         if username_info in list_of_files:
             file1 = open(username_info, 'rb')
-            verify = file1.read()
-            #verify = file1.read().splitlines()
-            #if password_info in verify:
+            verify = file1.read()    
             if bcrypt.checkpw(password_info, verify):
-                global logged_in_name
-                logged_in_name = username_info
+                self.logged_in.set(str(username_info))
                 self.show_frame(StartPage)
+
             else:
                 user_message.set('Password Not Recognised')
         else:
             user_message.set('User Not Found')
 
     def readfile(self, frame):
-        f = open('paulli', 'r')
+        file_to_open = self.logged_in.get() + 'li'
+        f = open(file_to_open, 'r')
         count = 0
 
         for line in f:
@@ -158,14 +148,46 @@ class PassHashApp(tk.Tk):
                 string = chr(ord(letter) + 12)
                 password_decrypt += string
 
-            website = tk.Label(frame, text=website_decrypt, bg=app_color) 
-            website.grid(row=2 + count, sticky='w')
-            login = tk.Label(frame, text=login_decrypt, bg=app_color) 
-            login.grid(row=2 + count, column=1)
-            password = tk.Label(frame, text=password_decrypt, bg=app_color)
-            password.grid(row=2 + count, column=2, sticky='e')
+            website_col = tk.Label(frame, text=website_decrypt, font=PAGE_FONT, bg=app_color) 
+            website_col.grid(row=2 + count, sticky='w')
+            login_col = tk.Label(frame, text=login_decrypt, font=PAGE_FONT, bg=app_color) 
+            login_col.grid(row=2 + count, column=1)
+            password_col = tk.Label(frame, text=password_decrypt, font=PAGE_FONT, bg=app_color)
+            password_col.grid(row=2 + count, column=2, sticky='e')
             count += 1
         f.close()
+
+    def add_entry(self, *args):
+        website_info = args[0].get()
+        login_info = args[1].get()
+        password_info = args[2].get()
+
+        website_encrypt = ''
+        login_encrypt = ''
+        password_encrypt = ''
+
+        for letter in website_info:
+            string = chr(ord(letter) - 12)
+            website_encrypt += string
+
+        for letter in login_info:
+            string = chr(ord(letter) - 12)
+            login_encrypt += string
+
+        for letter in password_info:
+            string = chr(ord(letter) - 12)
+            password_encrypt += string
+    
+        file_to_open = self.logged_in.get() + 'li'
+        file = open(file_to_open, 'a')
+        file.write(website_encrypt + ',' + login_encrypt + ',' + password_encrypt + ',\n')
+        file.close()
+
+        args[3].delete(0, 100)
+        args[4].delete(0, 100)
+        args[5].delete(0, 100)
+
+
 
 
 class RegisterPage(tk.Frame):
@@ -180,19 +202,23 @@ class RegisterPage(tk.Frame):
         username = tk.StringVar()
         password = tk.StringVar()
 
-        user_label = tk.Label(self, text='Username', bg=app_color)
+        user_label = tk.Label(self, text='Username', font=PAGE_FONT, bg=app_color)
         user_label.pack()
         user_entry = ttk.Entry(self, textvariable=username)
         user_entry.pack()
-        pass_label = tk.Label(self, text='Password', bg=app_color)
+        pass_label = tk.Label(self, text='Password', font=PAGE_FONT, bg=app_color)
         pass_label.pack()
         pass_entry = ttk.Entry(self, textvariable=password)
         pass_entry.pack(pady=10)
 
-        reg_button = ttk.Button(self, text='Register', command=lambda: controller.reg_user(username, password, user_entry, pass_entry))
+        reg_button = ttk.Button(self, text='Register', command=lambda: controller.reg_user(username, password, user_entry, pass_entry, register_message))
         reg_button.pack()
         back_button = ttk.Button(self, text='Back To Login', command=lambda: controller.show_frame(LoginPage))
         back_button.pack(side='bottom', pady=10)
+
+        register_message = tk.StringVar()
+        success_label = tk.Label(self, textvariable=register_message, font=PAGE_FONT, bg=app_color)
+        success_label.pack()
 
 class LoginPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -204,17 +230,17 @@ class LoginPage(tk.Frame):
         label = tk.Label(self, text='Welcome to PassHash\n All Your Passwords \n SECURE', font=PAGE_FONT, bg=app_color)
         label.pack(pady=20, padx=20)
 
-        label = tk.Label(self, text='Please enter your login details below', bg=app_color)
+        label = tk.Label(self, text='Please enter your login details below', font=PAGE_FONT, bg=app_color)
         label.pack()
 
         username = tk.StringVar()
         password = tk.StringVar()
 
-        user_label = tk.Label(self, text='Username', bg=app_color)
+        user_label = tk.Label(self, text='Username', font=PAGE_FONT, bg=app_color)
         user_label.pack()
         user_entry = ttk.Entry(self, textvariable=username)
         user_entry.pack()
-        pass_label = tk.Label(self, text='Password', bg=app_color)
+        pass_label = tk.Label(self, text='Password', bg=app_color, font=PAGE_FONT)
         pass_label.pack()
         pass_entry = ttk.Entry(self, textvariable=password)
         pass_entry.pack(pady=10)
@@ -225,7 +251,7 @@ class LoginPage(tk.Frame):
         button2.pack(pady=10, padx=20)
         
         user_message = tk.StringVar()
-        message_holder = tk.Label(self, textvariable=user_message, bg=app_color)
+        message_holder = tk.Label(self, textvariable=user_message, font=PAGE_FONT, bg=app_color)
         message_holder.pack(side='bottom')
 
 class StartPage(tk.Frame):
@@ -233,19 +259,19 @@ class StartPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         
         self.configure(bg=app_color)
+        
 
-        logged_in_message = 'Signed in as ' + logged_in_name
-
-        label = tk.Label(self, text='Welcome to PassHash\n All Your Passwords - SECURE', font=PAGE_FONT, bg=app_color)
-        label.pack(pady=20, padx=20)
-        labe2 = tk.Label(self, text=logged_in_message, font=PAGE_FONT, bg=app_color)
-        labe2.pack(pady=20, padx=20)
+        label = tk.Label(self, text='Welcome to PassHash\n Signed in as:', font=PAGE_FONT, bg=app_color)
+        label.pack(pady=10, padx=10)
+        
+        logged_in_label = tk.Label(self, textvariable=controller.logged_in, bg=app_color)
+        logged_in_label.pack()        
 
         button1 = ttk.Button(self, text='Password Generator', command=lambda: controller.show_frame(PasswordGenerator))
         button1.pack(pady=10, padx=20)
         button2 = ttk.Button(self, text='Password Vault', command=lambda: controller.show_frame(PasswordVault))
         button2.pack(pady=10, padx=20)
-        button3 = ttk.Button(self, text='Nothing - Yet')
+        button3 = ttk.Button(self, text='Add Login', command=lambda: controller.show_frame(AddLoginPage))
         button3.pack(pady=10, padx=20)
 
 class PasswordGenerator(tk.Frame):
@@ -284,7 +310,7 @@ class PasswordGenerator(tk.Frame):
         pass_holder = tk.Label(self, textvariable=password_text, bg=app_color, fg='#595959', font='Roboto')
         pass_holder.pack(padx=10, pady=10)
 
-        keep_button = ttk.Button(self, text="Keep Password", command=lambda: controller.show_frame(SetPassPage))
+        keep_button = ttk.Button(self, text="Keep Password", command=lambda: controller.show_frame(KeepPassPage))
         keep_button.pack()
 
         #Button
@@ -302,23 +328,25 @@ class PasswordVault(tk.Frame):
         
         label = tk.Label(self, text='Your Password Vault', font=PAGE_FONT, bg=app_color)
         label.pack(pady=10, padx=10)
-        frame1 = ttk.Frame(self, height=350, width=200)
+        frame1 = tk.Frame(self, height=350, width=200, bg=app_color)
         frame1.pack()
-        entity_label = tk.Label(frame1, text='Your Passwords', bg=app_color)
+        entity_label = tk.Label(frame1, text='Your Passwords', font=PAGE_FONT, bg=app_color)
         entity_label.grid(columnspan=3, row=0)
-        website_label = tk.Label(frame1, text='Website: ', bg=app_color)
-        login_label = tk.Label(frame1, text='Login: ', bg=app_color)
-        password_label = tk.Label(frame1, text='Password: ', bg=app_color)
+        website_label = tk.Label(frame1, text='Website: ', font=PAGE_FONT, bg=app_color)
+        login_label = tk.Label(frame1, text='Login: ', font=PAGE_FONT, bg=app_color)
+        password_label = tk.Label(frame1, text='Password: ', font=PAGE_FONT, bg=app_color)
         website_label.grid(row=1)
         login_label.grid(row=1, column=1)
         password_label.grid(row=1, column=2)
 
-        controller.readfile(frame1)
-
-        home_button = ttk.Button(self, text='Back to Home', command=lambda: controller.show_frame(StartPage))
+        home_button = tk.Button(self, text='Go Home', command=lambda: controller.show_frame(StartPage))
         home_button.pack(side='bottom', pady=10)
-
-class SetPassPage(tk.Frame):
+        add_button = ttk.Button(self, text='Add a Login', command=lambda: controller.show_frame(AddLoginPage))
+        add_button.pack(side='bottom', pady=10)
+        obtain_button = ttk.Button(self, text='Obtain Passcodes', command=lambda: controller.readfile(frame1))
+        obtain_button.pack(side='bottom', pady=10)
+        
+class KeepPassPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
@@ -329,6 +357,38 @@ class SetPassPage(tk.Frame):
 
         home_button = ttk.Button(self, text='Back to Home', command=lambda: controller.show_frame(StartPage))
         home_button.pack(side='bottom')
+
+class AddLoginPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        
+        self.configure(bg=app_color)
+
+        label = tk.Label(self, text='Register Page', font=PAGE_FONT, bg=app_color)
+        label.pack(pady=20, padx=20)
+
+        website = tk.StringVar()
+        login = tk.StringVar()
+        password = tk.StringVar()
+
+
+        website_label = tk.Label(self, text='Website:', bg=app_color)
+        website_label.pack()
+        website_entry = ttk.Entry(self, textvariable=website)
+        website_entry.pack()
+        login_label = tk.Label(self, text='Login', bg=app_color)
+        login_label.pack()
+        login_entry = ttk.Entry(self, textvariable=login)
+        login_entry.pack(pady=10)
+        password_label = tk.Label(self, text='Password', bg=app_color)
+        password_label.pack()
+        password_entry = ttk.Entry(self, textvariable=password)
+        password_entry.pack(pady=10)
+
+        add_button = ttk.Button(self, text='Add Login', command=lambda: controller.add_entry(website, login, password, website_entry, login_entry, password_entry))
+        add_button.pack()
+        back_button = ttk.Button(self, text='Back Home', command=lambda: controller.show_frame(StartPage))
+        back_button.pack(side='bottom', pady=10)
 
 
 app = PassHashApp()
